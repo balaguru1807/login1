8b075c3b0fd4186257684c11014b4225d6dd7933
balaguru1807 <balaguru.k.p.s1807@gmail.com>
2026-01-19 12:02:58 +0530

    extra message

--- src/login.js
+++ src/login.js
@@ -14,7 +14,7 @@ function Login() {
   const handleSubmit = async e => {
     e.preventDefault();
     await axios.post("http://localhost:5000/login", form);
-    alert("Login info are sent to backend");
+    alert("login successfully sent to backend succsessfully");
   };
 
   return (
